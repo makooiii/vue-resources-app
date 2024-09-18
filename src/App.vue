ee516291088b276be1bb60c503dc478cef1b17cf
Mark Sapiandante <msapiandante.ccheetah@gmail.com>
2024-09-18 17:00:44 +0800

    adding dynamic component

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,23 @@
 <template>
-  <the-header title="Rememberme"></the-header>
+  <the-header title="Remember Me"></the-header>
   <stored-resources :resources="storedResources"></stored-resources>
+  <the-resources></the-resources>
 </template>
 
 <script>
-import StoredResources from './components/learing-resources/StoredResources.vue';
 import TheHeader from './components/layouts/TheHeader'
+import TheResources from './components/learing-resources/TheResources.vue';
 export default {
   components: {
-    StoredResources,
     TheHeader,
-  },
-  data() {
-    return {
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official VueJS documentation',
-          link: 'https://vuejs.org',
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn to google..',
-          link: 'https://www.google.com',
-        },
-      ],
-    };
+    TheResources
   },
 };
 </script>
 
 
 
-<style scoped>
+<style>
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
 
 * {
